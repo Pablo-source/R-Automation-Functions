9bb1df246563f9cf00ef482ad9c33a5151fabeb0
Pablo Leon <datavizvalencia@gmail.com>
2022-06-26 17:59:46 +0100

    Update script
    Updated script to make it more efficient

--- 00 Setup project structure.R
+++ 00 Setup project structure.R
@@ -1,24 +1,29 @@
 
 # 00 Setup project structure
-# 11/04/2022
+# Updated on 24/06/2022
 # AIM: At the start of any new report you design first you create a brand new Project Folder to host all the scripts alongside input and output files
 #      Then the next action you take is to create a Folder structure within that Project folder to organise your workflow and R scripts
 
 
 # FUNCTION TO SETUP PROJECT STRUCTURE WITH FOLDERS AND SUB FOLDERS
+# Updated scripts to include here() function
+
+# I have created these two files using relative paths (if you execute this code in your machine,
+# (after creating a project and also executing the function below): It will work on any laptop 
+
+library(here)
 
 project_setup <-function(){
   
-  if(!dir.exists("data")){dir.create("data")}
-  if(!dir.exists("Output")){dir.create("Output")}
-  if(!dir.exists("Shapefiles")){dir.create("Shapefiles")}
-  if(!dir.exists("Checks")){dir.create("Checks")}
-  if(!dir.exists("Maps")){dir.create("Maps")}
-  if(!dir.exists("Archive")){dir.create("Archive")}
-  #Create sub-folders within folders
-  if(dir.exists("Archive")){dir.create("/home/pablo/Documents/R desktop/R Warehouse/Data wrangling/Data wrangling/Project_01/subfolder_name")}
-  #if(dir.exists("Archive")){dir.create(~/Archive."Archive_Sub")}
-  #if(!dir.exists(".Archive/Test_subf")){dir.create(file.path(getwd(),"subfolder"), recursive = TRUE)}  
+  if(!dir.exists("data")){dir.create(here::here("data"))}
+  if(!dir.exists("Output")){dir.create(here::here("Output"))}
+  if(!dir.exists("Shapefiles")){dir.create(here::here("Shapefiles"))}
+  if(!dir.exists("Checks")){dir.create(here::here("Checks"))}
+  if(!dir.exists("Maps")){dir.create(here::here("Maps"))}
+  if(!dir.exists("Archive")){dir.create(here::here("Archive"))}
+  #Create sub-folders within folders. This sub folder is nested under Archive folder
+  if(dir.exists("Archive")){dir.create(here::here("/home/pablo/Documents/Pablo/Rprojects/Rwarehouse/Archive/subfolder_new"))}
+
 } 
 
 # Run code below to use function and create folder structure
