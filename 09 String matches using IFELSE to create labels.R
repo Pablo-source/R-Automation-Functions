f557ede8ae75f0dbdbb462f01e263a5508b8e454
Pablo Leon <datavizvalencia@gmail.com>
2022-08-01 08:35:11 +0100

    Add files via upload
    Sting matches to create labels using nested IFELSE statements

---
+++ 09 String matches using IFELSE to create labels.R
@@ -0,0 +1,27 @@
+# 3 Split main dataset by organisaion types (CCG, TRUST, STP)
+# 
+# The grepl() stands for “grep logical”. 
+# In R it is a built-in function that searches for matches 
+# of a string or string vector. The grepl() method takes a pattern 
+# and data and returns TRUE if a string contains the pattern, otherwise FALSE.
+
+# We use ifelse() nested conditions to flag each sting match provided by grepl function
+# and assign it the corresponding organisation label description
+
+Torgrc <- IMPRDASH %>% 
+  select(OrgName) %>% 
+  mutate(
+    ORGTYPE = ifelse(grepl('CCG',OrgName),'CCG',
+                     (ifelse(grepl('Trust',OrgName),'TRUST',
+                             (ifelse(grepl('STP',OrgName),'STP',0)))))
+  )
+IMPRDASI <-IMPRDASH %>% 
+  select(OrgName,OrgType,Measure,SubCompartment,Value,ReportingDate,Frequency,Quarter,STPCode) %>% 
+  mutate(
+    ORGTYPE = ifelse(grepl('CCG',OrgName),'CCG',
+                     (ifelse(grepl('Trust',OrgName),'TRUST',
+                             (ifelse(grepl('STP',OrgName),'STP',0)))))
+  )
+IMP_STP <- IMPRDASI %>% filter(ORGTYPE=='STP')
+IMP_CCG <- IMPRDASI %>% filter(ORGTYPE=='CCG')
+IMP_TRUST <- IMPRDASI %>% filter(ORGTYPE=='TRUST')
