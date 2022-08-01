9313340e54ff2fb57af41365a1ad5a9d5d852224
Pablo Leon <datavizvalencia@gmail.com>
2022-08-01 08:18:47 +0100

    Add files via upload
    Using Sys.glob() function you can list file extension types in project folder

---
+++ 06 List project content by file extension.R
@@ -0,0 +1,8 @@
+# List files extensions in a project folder 
+filenames <- Sys.glob("*.dbf")
+# List R files
+filenames <- Sys.glob("*.R")
+# List .xlsx  files
+filenames <- Sys.glob("*.xlsx")
+# List .csv  files
+filenames <- Sys.glob("*.csv") 
