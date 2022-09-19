b15adfec46338f4855bfe817c16777f82a6c2dab
Pablo Leon <datavizvalencia@gmail.com>
2022-09-19 13:56:21 +0100

    Add files via upload
    List folder content by file type

---
+++ 21 List folder content by file type.R
@@ -0,0 +1,11 @@
+# 1.3 List files in a Sub-directory with specific file extension
+
+# R Script: 21 List folder content by file type.R
+
+# List excel files on Data sub-directory
+list.files (path = "./Data" ,pattern = "xlsx$")
+
+# List other file type in  Data folder
+list.files (path = "./Data" ,pattern = "R$")
+list.files (path = "./Data" ,pattern = "Rdata$")
+list.files (path = "./Data" ,pattern = "txt$")
