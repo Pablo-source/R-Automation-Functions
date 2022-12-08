a23a67b2ce40e1a2255280ad105d149e6cb3da0a
Pablo Leon <datavizvalencia@gmail.com>
2022-12-08 09:06:08 +0000

    Update 03 Install and load several packages at once.R
    Update with pacman library examples

--- 03 Install and load several packages at once.R
+++ 03 Install and load several packages at once.R
@@ -1,8 +1,13 @@
 # Installing new required packages for "02 Parallel processing.Rmd"
 
-# Install several packages at once
+# 1-2 Install several packages at once
 install.packages(c("dimRed","Rspectra","igraph"),dependencies = TRUE)
 
-# Load severa packages at once 
+# 2-2 Load severa packages at once 
 my_packages <- c("dimRed", "Rspectra", "igraph")
 lapply(my_packages, require, character.only = TRUE)    # Load multiple packages
+
+# A MORE EFFICIENT WAY USING pacman package
+# Another efficient way is using PACMAN package we can install several packages at once using p_load() function
+# use p_load() to list in this function the set of required packages for your project
+pacman::p_load(here,dplyr,sf,raster,mapdata,ggplot2)
