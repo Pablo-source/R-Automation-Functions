c535c57a6ebd476cf97c3517b122a80adf5dac67
Pablo Leon <datavizvalencia@gmail.com>
2023-01-21 11:01:05 +0000

    Update 00 Setup project structure.R
    Update

--- 00 Setup project structure.R
+++ 00 Setup project structure.R
@@ -4,29 +4,16 @@
 # AIM: At the start of any new report you design first you create a brand new Project Folder to host all the scripts alongside input and output files
 #      Then the next action you take is to create a Folder structure within that Project folder to organise your workflow and R scripts
 
-
-# FUNCTION: SETUP Folders and Sub Folders in a new brand PROJECT
-
-
-
-
-# Updated scripts to include here() function
-
-# I have created these two files using relative paths (if you execute this code in your machine,
-# (after creating a project and also executing the function below): It will work on any laptop 
-
-library(here)
-
 project_setup <-function(){
   
-  if(!dir.exists("data")){dir.create(here::here("data"))}
-  if(!dir.exists("Output")){dir.create(here::here("Output"))}
-  if(!dir.exists("Shapefiles")){dir.create(here::here("Shapefiles"))}
-  if(!dir.exists("Checks")){dir.create(here::here("Checks"))}
-  if(!dir.exists("Maps")){dir.create(here::here("Maps"))}
-  if(!dir.exists("Archive")){dir.create(here::here("Archive"))}
+  if(!dir.exists("data")){dir.create("data")}
+  if(!dir.exists("Output")){dir.create("Output")}
+  if(!dir.exists("Shapefiles")){dir.create("Shapefiles")}
+  if(!dir.exists("Checks")){dir.create("Checks")}
+  if(!dir.exists("Maps")){dir.create("Maps")}
+  if(!dir.exists("Archive")){dir.create("Archive")}
   #Create sub-folders within folders. This sub folder is nested under Archive folder
-  if(dir.exists("Archive")){dir.create(here::here("/home/pablo/Documents/Pablo/Rprojects/Rwarehouse/Archive/subfolder_new"))}
+  if(dir.exists("Archive")){dir.create(("../Rprojects/Rwarehouse/Project/subfolder_new"))}
 
 } 
 
