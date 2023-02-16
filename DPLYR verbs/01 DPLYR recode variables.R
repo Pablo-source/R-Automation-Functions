9c68374ab2df320282b83ee2c788e141ce336a7d
Pablo Leonrodenas <pablo.leonrodenas@england.nhs.uk>
2023-02-16 08:11:22 +0000

    Updated functions to run today

---
+++ DPLYR verbs/01 DPLYR recode variables.R
@@ -0,0 +1,39 @@
+# 01 DPLYR recode variables.R
+
+## Recode long winding variable names.simple DPLYR recode example 
+gimmeCaffeine <- gimmeCaffeine %>%
+  mutate(origin = recode(origin, 
+                         "colombia" = "Colombian", 
+                         "ethiopia" = "Ethiopian",
+                         "peru"     = "Peruvian"))
+
+
+# Example of long variable names, how to recede them into shorter names
+Data02_short <- Data02 %>% 
+  mutate(
+    Indic_name_sh = recode(
+      Indic_name, 
+      "Alcohol related liver disease - Admissions per 100,000 age-sex weighted population (excluding zero-day non-elective admissions)" = "Alcohol liver disease",
+      "Cerebrovascular disease (10B) - Total spend per 1,000 age-sex weighted population" = "Cerebro",
+      "Falls & fractures : Hip fractures per 100,000 age-sex weighted population aged 65-79" = "Falls"),
+    
+    Grogr_sh = recode(
+      GeographyName,
+      "HospitalA Longname" = "HospitalA",
+      "HospitalB Longname" = "HospitalB",
+      "HospitalC Longname" = "HospitalC",
+      "HospitalD Longname" = "HospitalD",
+      "HospitalE Longname" = "HospitalE",
+      "HospitalF Longname"  = "HospitalF",
+      "HospitalG Longname" = "HospitalG"
+    )     
+  ) %>% 
+  select(Indic_name_sh,Grogr_sh,Value) 
+
+Data02_short
+
+Heatmap03 <- Data02_short %>% 
+  ggplot(aes(Grogr_sh, Indic_name_sh, fill= Value)) + 
+  geom_tile() +
+  ggtitle("Alcohol, Cerebrovasular, and Fall and Fractures Indicators, Short label names") 
+Heatmap03
---
+++ DPLYR verbs/02 DPLYR subset rows percentage IN.R
@@ -0,0 +1,16 @@
+# 01 DPLYR subset rows percentage IN.R
+# filter() and str_detect() to subset rows using Regular Expressions 
+
+library(dplyr)
+ 
+df <- data.frame(ALdays = c(31, 21, 2, 5, 12, 31, 4, 3,12,18,21,22), 
+                 name = c("Marc", "Ann", "Marc","Lisa","Ann","Tom","Chris", "Lisa", "Kyla", "Tom", "Lynn", "Lynn"))
+
+
+# Subset rows  containing information for Marc and Ann
+Subset <-c("Marc","Ann")
+
+df_sel <- df %>% 
+          filter(name %in% Subset)
+
+df_sel
