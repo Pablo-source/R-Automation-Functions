4ba3f2f195213f6e5cfbb5827548f1fa069af5a5
Pablo Leon <datavizvalencia@gmail.com>
2023-11-03 08:31:39 +0000

    Update 17 Importing Excel files into R.R

--- 17 Importing Excel files into R.R
+++ 17 Importing Excel files into R.R
@@ -31,6 +31,20 @@ excel_sheets("./data/My_Excel_input_file_2022.xlsx")
 # This returns the number of tabs and we now know that we want to import the third sheet
  
 # Step 03 04: import  data by using read_excel() main function alongside clean_names() function from janitor package
+# Consider also using several of these arguments when importing Excel files into R using read_excel_function()
+# range	
+# A cell range to read from, as described in cell-specification. Includes typical Excel ranges like "B3:D87", possibly including the sheet name like "Budget!B2:G14", and more. Interpreted strictly, even if the range forces the inclusion of leading or trailing empty rows or columns. Takes precedence over skip, n_max and sheet.
+# col_names	
+# TRUE to use the first row as column names, FALSE to get default names, or a character vector giving a name for each column. If user provides col_types as a vector, col_names can have one entry per column, i.e. have the same length as col_types, or one entry per unskipped column.
+# col_types	
+# Either NULL to guess all from the spreadsheet or a character vector containing one entry per column from these options: "skip", "guess", "logical", "numeric", "date", "text" or "list". If exactly one col_type is specified, it will be recycled. The content of a cell in a skipped column is never read and that column will not appear in the data frame output. A list cell loads a column as a list of length 1 vectors, which are typed using the type guessing logic from col_types = NULL, but on a cell-by-cell basis.
+# na	
+# Character vector of strings to interpret as missing values. By default, readxl treats blank cells as missing data.
+# trim_ws	
+# Should leading and trailing whitespace be trimmed?
+# skip	
+# Minimum number of rows to skip before reading anything, be it column names or data. Leading empty rows are automatically skipped, so this is a lower bound. Ignored if range is given.
+
 datan <- read_excel(here("data", "My_Excel_input_file_2022.xlsx"), sheet = 3) %>% 
          clean_names()
 datan
